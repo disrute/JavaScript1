db41f75ea8796ece68b2d95ad5f76fcf4980c077
lo den <lochlan@appswiz.com>
2019-10-15 20:56:51 +1100

    Completed Validator

--- Week3/homework/CCValidator.js
+++ Week3/homework/CCValidator.js
@@ -3,6 +3,8 @@ function validator(ccNumber) {
     // length must be 16
     if (ccString.length < 16 || ccString.length > 16) {
         console.log(`Credit Card No. length is incorrect. Length is ${ccString.length}`);
+    } else {
+        console.log(`CC Number length is ${ccString.length}`);
     }
     
     // test length is 16
@@ -22,29 +24,24 @@ function validator(ccNumber) {
     // Then backreference with \1{15}$/
     let multipleDigits =/^(\d)\1{15}$/.test(ccString);
     if (multipleDigits === false) {
-        console.log('The CC Number has more than one digit.');
+        console.log('The CC Number has at least 2 unique digits.');
         //console.log('More than one digit? ' + !multipleDigits);
     }
 
     // Test if the final digit is even
     let endRegex = /\d$/;
     const lastDigit = parseInt(endRegex.exec(ccString));
-    console.log(lastDigit);
 
-    
-    
+    lastDigit % 2 === 0 ? console.log(`The last digit is ${lastDigit}, which is even.`) : console.log('The last digit is odd.');
 
-
-
-
-    // the sum of all digits must be > 16
-    /*for (var i = 0; i < ccNumber.length; i++) {
-        let sum = 0;
-        sum = sum + ccNumber[i];
-        console.log(sum);
-    }*/
+    // Sum of all digits must equate to 16.
+    sum = 0;
+    for (let i = 0; i < ccString.length; i++) {
+        sum += parseInt(ccString[i], 10);
+    }
+    sum > 16 ? console.log(`Sum of CC Num is ${sum}, therefore it\'s valid.`) : console.log('The CC Num is not greater than 16, it is invalid.');
 }
 
 //validator(1111111111111111);
-validator(1111211111111111);
+validator(1111211111111112);
 //validator('143904819');
